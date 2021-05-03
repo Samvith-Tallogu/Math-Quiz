cae70688027fc71c2fc3b4290b5dbe203c55d762
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-05-03 18:57:54 +0530

    corrected error once again

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -5,7 +5,7 @@ function send(){
 
     question_number = "<h4>" + number1 + "X" + number2 +"</h4>";
     input_box = "<br> Answer: <input type = 'text' id='input_check_box'>";
-    check_button = "<br><br> <button class='btn btn-info'> onclick='check()'>Check</button> ";
+    check_button = "<br><br> <button class='btn btn-info' onclick='check()'>Check</button> ";
     row = question_number + check_button ;
 
     document.getElementById("output").innerHTML = row;
